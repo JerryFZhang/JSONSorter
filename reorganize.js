ab67512fa5334d4fd6be6779e54d53f76b75443d
zhang96 <jerry.fengwei@gmail.com>
2016-12-01 17:09:14 -0500

    Init

---
+++ reorganize.js
@@ -0,0 +1,69 @@
+var fs = require('fs');
+var json;
+var fileName = '/paraData.json';
+//for (var i = 0; i < fileList.length; i++) {
+//    var fileName = fileList[i];
+//    while(fileName == undefined){
+//        fileName = toString(fileList[i]);
+//        console.log(fileName);
+//    }
+//    console.log(fileName);
+//    fileName = '/' + fileName;
+//    console.log(fileName);
+fs.readFile(__dirname+'\/\paraData.json', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    obj = JSON.parse(data);
+    var outputString;
+    var objectNameList = []
+    for (var i = 0; i < obj.data.length; i++) {
+        outputString += '{ "HeartRate": "'
+        outputString += obj.data[i].HeartRate
+        outputString += '", "UprightRow": "'
+        outputString += obj.data[i].UprightRow
+        outputString += '", "TrunkFlexion": "'
+        outputString += obj.data[i].TrunkFlexion
+        outputString += '", "FirstName": "'
+        outputString += obj.data[i].FirstName
+        outputString += '", "InclineBenchPress": "'
+        outputString += obj.data[i].InclineBenchPress
+        outputString += '", "MaxVO2": "'
+        outputString += obj.data[i].MaxVO2
+        outputString += '", "StrengthPercentile": "'
+        outputString += obj.data[i].StrengthPercentile
+        outputString += '", "Gender": "'
+        outputString += obj.data[i].Gender
+        outputString += '", "Weight": "'
+        outputString += obj.data[i].Weight
+        outputString += '", "Year": "'
+        outputString += obj.data[i].Year
+        outputString += '", "ArmCurl": "'
+        outputString += obj.data[i].ArmCurl
+        outputString += '", "Stage": "'
+        outputString += obj.data[i].Stage
+        outputString += '", "LastName": "'
+        outputString += obj.data[i].LastName
+        outputString += '", "Client": "'
+        outputString += obj.data[i].Client
+        outputString += '", "TFPercentile": "'
+        outputString += obj.data[i].TFPercentile
+        outputString += '", "Age": "'
+        outputString += obj.data[i].Age
+        outputString += '", "TFOverride": "'
+        outputString += obj.data[i].TFOverride
+        outputString += '", "DeadLift": "'
+        outputString += obj.data[i].DeadLift
+        outputString += '", "MVO2Percentile": "'
+        outputString += obj.data[i].MVO2Percentile
+        outputString += '"},';
+    }
+    fs.writeFile(__dirname, outputString, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(obj.data.length + 'sets of data written.')
+    });
+    //    console.log(outputString);
+});
+//}
